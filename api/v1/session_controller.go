e6f6c4eea2b4738de76dc961f0925af59108a9fc
wudiqiegaoleng <746894206@qq.com>
2026-04-27 18:41:28 +0800

    GetUserSessionList 获取用户会话列表 接口完成

--- api/v1/session_controller.go
+++ api/v1/session_controller.go
@@ -1,1 +1,43 @@
 package v1
+
+import (
+	"kama-chat-server/internal/dto/request"
+	"kama-chat-server/internal/service/gorm"
+	"kama-chat-server/pkg/constants"
+	"kama-chat-server/pkg/zlog"
+
+	"github.com/gin-gonic/gin"
+
+	"net/http"
+)
+
+// OpenSession 打开会话
+func OpenSession(c *gin.Context) {
+    var openSessionReq request.OpenSessionRequest
+    if err := c.BindJSON(&openSessionReq); err != nil {
+        zlog.Error(err.Error())
+        c.JSON(http.StatusOK, gin.H{
+            "code":    500,
+            "message": constants.SYSTEM_ERROR,
+        })
+        return
+    }
+    message, sessionId, ret := gorm.SessionService.OpenSession(openSessionReq)
+    JsonBack(c, message, ret, sessionId)
+}
+
+// GetUserSessionList 获取用户会话列表
+func GetUserSessionList(c *gin.Context) {
+    var getUserSessionListReq request.OwnlistRequest
+    if err := c.BindJSON(&getUserSessionListReq); err != nil {
+        zlog.Error(err.Error())
+        c.JSON(http.StatusOK, gin.H{
+            "code": 500,
+            "message": constants.SYSTEM_ERROR,
+        })
+        return
+    }
+    message, sessionList, ret := gorm.SessionService.GetUserSessionList(getUserSessionListReq.OwnerId)
+    JsonBack(c, message, ret, sessionList)
+
+}
--- internal/service/gorm/session_service.go
+++ internal/service/gorm/session_service.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"kama-chat-server/internal/dao"
 	"kama-chat-server/internal/dto/request"
+	"kama-chat-server/internal/dto/respond"
 	"kama-chat-server/internal/model"
 	myredis "kama-chat-server/internal/service/redis"
 	"kama-chat-server/pkg/constants"
@@ -35,20 +36,20 @@ func (s *sessionService) OpenSession(req request.OpenSessionRequest) (string, st
 				if errors.Is(res.Error, gorm.ErrRecordNotFound) {
 					zlog.Info("会话没有找到，将新建会话")
 					createReq := request.CreateSessionRequest{
-					SendId:    req.SendId,
-					ReceiveId: req.ReceiveId,
-				}
+						SendId:    req.SendId,
+						ReceiveId: req.ReceiveId,
+					}
 
 					return s.CreateSession(createReq)
 				}
 			}
-			rspString, err := json.Marshal(session)
-			if err != nil {
-				zlog.Error(err.Error())
-			}
-			if err := myredis.SetKeyEx("session_"+req.SendId+"_"+req.ReceiveId+"_"+session.Uuid, string(rspString), time.Minute*constants.REDIS_TIMEOUT); err != nil {
-				zlog.Error(err.Error())
-			}
+			//rspString, err := json.Marshal(session)
+			//if err != nil {
+			//	zlog.Error(err.Error())
+			//}
+			//if err := myredis.SetKeyEx("session_"+req.SendId+"_"+req.ReceiveId+"_"+session.Uuid, string(rspString), time.Minute*constants.REDIS_TIMEOUT); err != nil {
+			//	zlog.Error(err.Error())
+			//}
 			return "会话创建成功", session.Uuid, 0
 		} else {
 			zlog.Error(err.Error())
@@ -64,62 +65,106 @@ func (s *sessionService) OpenSession(req request.OpenSessionRequest) (string, st
 	return "会话创建成功", session.Uuid, 0
 }
 
-
-
 // CreateSession 创建会话
 func (s *sessionService) CreateSession(req request.CreateSessionRequest) (string, string, int) {
 	var user model.UserInfo
-    if res := dao.GormDB.Where("uuid = ?", req.SendId).First(&user); res.Error != nil {
-        zlog.Error(res.Error.Error())
-        return constants.SYSTEM_ERROR, "", -1
-    }
-    var session model.Session
+	if res := dao.GormDB.Where("uuid = ?", req.SendId).First(&user); res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, "", -1
+	}
+	var session model.Session
 	value, err := random.GetNowAndLenRandomString(11)
 	if err != nil {
 		zlog.Error(err.Error())
 		return constants.SYSTEM_ERROR, "", -1
 	}
 	session.Uuid = fmt.Sprintf("S%s", value)
-    session.SendId = req.SendId
-    session.ReceiveId = req.ReceiveId
-    session.CreatedAt = time.Now()
-    if req.ReceiveId[0] == 'U' {
-        var receiveUser model.UserInfo
-        if res := dao.GormDB.Where("uuid = ?", req.ReceiveId).First(&receiveUser); res.Error != nil {
-            zlog.Error(res.Error.Error())
-            return constants.SYSTEM_ERROR, "", -1
-        }
-        if receiveUser.Status == user_status_enum.DISABLE {
-            zlog.Error("该用户被禁用了")
-            return "该用户被禁用了", "", -2
-        } else {
-            session.ReceiveName = receiveUser.Nickname
-            session.Avatar = receiveUser.Avatar
-        }
-    } else {
-        var receiveGroup model.GroupInfo
-        if res := dao.GormDB.Where("uuid = ?", req.ReceiveId).First(&receiveGroup); res.Error != nil {
-            zlog.Error(res.Error.Error())
-            return constants.SYSTEM_ERROR, "", -1
-        }
-        if receiveGroup.Status == group_status_enum.DISABLE {
-            zlog.Error("该群聊被禁用了")
-            return "该群聊被禁用了", "", -2
-        } else {
-            session.ReceiveName = receiveGroup.Name
-            session.Avatar = receiveGroup.Avatar
-        }
-    }
+	session.SendId = req.SendId
+	session.ReceiveId = req.ReceiveId
+	session.CreatedAt = time.Now()
+	if req.ReceiveId[0] == 'U' {
+		var receiveUser model.UserInfo
+		if res := dao.GormDB.Where("uuid = ?", req.ReceiveId).First(&receiveUser); res.Error != nil {
+			zlog.Error(res.Error.Error())
+			return constants.SYSTEM_ERROR, "", -1
+		}
+		if receiveUser.Status == user_status_enum.DISABLE {
+			zlog.Error("该用户被禁用了")
+			return "该用户被禁用了", "", -2
+		} else {
+			session.ReceiveName = receiveUser.Nickname
+			session.Avatar = receiveUser.Avatar
+		}
+	} else {
+		var receiveGroup model.GroupInfo
+		if res := dao.GormDB.Where("uuid = ?", req.ReceiveId).First(&receiveGroup); res.Error != nil {
+			zlog.Error(res.Error.Error())
+			return constants.SYSTEM_ERROR, "", -1
+		}
+		if receiveGroup.Status == group_status_enum.DISABLE {
+			zlog.Error("该群聊被禁用了")
+			return "该群聊被禁用了", "", -2
+		} else {
+			session.ReceiveName = receiveGroup.Name
+			session.Avatar = receiveGroup.Avatar
+		}
+	}
 
-    if res := dao.GormDB.Create(&session); res.Error != nil {
-        zlog.Error(res.Error.Error())
-        return constants.SYSTEM_ERROR, "", -1
-    }
-    if err := myredis.DelKeysWithPattern("group_session_list_" + req.SendId); err != nil {
-        zlog.Error(err.Error())
-    }
-    if err := myredis.DelKeysWithPattern("session_list_" + req.SendId); err != nil {
-        zlog.Error(err.Error())
-    }
-    return "会话创建成功", session.Uuid, 0
+	if res := dao.GormDB.Create(&session); res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, "", -1
+	}
+	if err := myredis.DelKeysWithPattern("group_session_list_" + req.SendId); err != nil {
+		zlog.Error(err.Error())
+	}
+	if err := myredis.DelKeysWithPattern("session_list_" + req.SendId); err != nil {
+		zlog.Error(err.Error())
+	}
+	return "会话创建成功", session.Uuid, 0
+}
+
+// GetUserSessionList 获取用户会话列表
+func (s *sessionService) GetUserSessionList(ownerId string) (string, []respond.UserSessionListRespond, int) {
+	rspString, err := myredis.GetKeyNilIsErr("session_list_" + ownerId)
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			var sessionList []model.Session
+			if res := dao.GormDB.Order("created_at DESC").Where("send_id = ?", ownerId).Find(&sessionList); res.Error != nil {
+				if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+					zlog.Info("未创建用户会话")
+					return "未创建用户会话", nil, 0
+				} else {
+					zlog.Error(res.Error.Error())
+					return constants.SYSTEM_ERROR, nil, -1
+				}
+			}
+			var sessionListRsp []respond.UserSessionListRespond
+			for i := 0; i < len(sessionList); i++ {
+				if sessionList[i].ReceiveId[0] == 'U' {
+					sessionListRsp = append(sessionListRsp, respond.UserSessionListRespond{
+						SessionId: sessionList[i].Uuid,
+						Avatar:    sessionList[i].Avatar,
+						UserId:    sessionList[i].ReceiveId,
+						Username:  sessionList[i].ReceiveName,
+					})
+				}
+			}
+			rspString, err := json.Marshal(sessionListRsp)
+			if err != nil {
+				zlog.Error(err.Error())
+			}
+			if err := myredis.SetKeyEx("session_list_"+ownerId, string(rspString), time.Minute*constants.REDIS_TIMEOUT); err != nil {
+				zlog.Error(err.Error())
+			}
+			return "获取成功", sessionListRsp, 0
+		} else {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, nil, -1
+		}
+	}
+	var rsp []respond.UserSessionListRespond
+	if err := json.Unmarshal([]byte(rspString), &rsp); err != nil {
+		zlog.Error(err.Error())
+	}
+	return "获取成功", rsp, 0
 }
